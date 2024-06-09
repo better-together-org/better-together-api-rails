a4f10becde294813d937729e8bad06e421acfc2e
Robert Smith <rob@bettertogethersolutions.com>
2024-06-08 23:22:36 -0230

    fix (temp): compile assets in prod if missing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,7 +30,7 @@ Rails.application.configure do # rubocop:todo Metrics/BlockLength
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
   config.assets.digest = true
   config.assets.enabled = true
 
